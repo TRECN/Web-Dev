96dd3b674569f03c98035ccc349b22bd443d9522
TRECN <rishabh1103@outlook.com>
2022-01-13 20:47:18 +0530

    commit

--- snakeGame/script.js
+++ snakeGame/script.js
@@ -10,7 +10,12 @@ class Snake{
     }
     move(){
         var newRect;
-      
+        if(this.rotateX==1){
+            newRect={
+                x:this.tail[this.tail.length-1].x+this.size,
+                y:this.tail[this.tail.length-1].y
+
+            }
         }else if(this.rotateX==-1){
             newRect={
                 x:this.tail[this.tail.length-1].x-this.size,
