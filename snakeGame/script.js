cd8fe842290f76be9d056f3ca9e21cf0f6593895
TRECN <rishabh1103@outlook.com>
2022-01-13 20:47:03 +0530

    commit

--- snakeGame/script.js
+++ snakeGame/script.js
@@ -1,12 +1,7 @@
 
 class Snake{
     constructor(x,y,size){
-        this.x=x;
-        this.y=y;
-        this.size=size;
-        this.tail=[{x:this.x,y:this.y}];
-        this.rotateX=0;
-        this.rotateY=1;
+his.rotateY=1;
     }
     move(){
         var newRect;
