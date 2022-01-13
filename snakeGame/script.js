898e23be08f74764147818a94cef79355916c2d8
TRECN <rishabh1103@outlook.com>
2022-01-13 20:48:00 +0530

    commit

--- snakeGame/script.js
+++ snakeGame/script.js
@@ -45,13 +45,7 @@ class Apple{
         var isTouching;
         while(true){
             isTouching=false;
-            this.x=Math.floor(Math.random()*canvas.width/snake.size)*snake.size
-            this.y=Math.floor(Math.random()*canvas.height/snake.size)*snake.size
-            for(var i=0;i<snake.tail.length;i++){
-                if(this.x==snake.tail[i].x&&this.y==snake.tail[i].y){
-                    isTouching=true
-                }
-            }
+      
             
             this.color = "pink"
             this.size=snake.size
