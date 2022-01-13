88d1fc027c005339ae4293e3849fb2821378b00b
TRECN <rishabh1103@outlook.com>
2022-01-13 19:46:46 +0530

    commit

--- snakeGame/script.js
+++ snakeGame/script.js
@@ -9,8 +9,13 @@ class Snake{
         this.rotateY=1;
     }
     move(){
+        var newRect;
         if(this.rotateX==1){
-            
+            newRect={
+                x:this.tail[this.tail.length-1].x+this.size,
+                y:this.taile[this.tail.length-1].y;
+
+            }
         }
     }
 }
